331a34829acba20dbdb6d16832f2b785b613c8ec
yusufaligokselsupplementler <37908301+yusufaligoksel@users.noreply.github.com>
2021-09-02 09:59:26 +0300

    .

--- JwtTokenLearning/JwtTokenLearning.Business/Services/Concrete/TokenService.cs
+++ JwtTokenLearning/JwtTokenLearning.Business/Services/Concrete/TokenService.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JwtTokenLearning.Business.Configuration;
+using JwtTokenLearning.Business.Dto;
+using JwtTokenLearning.Business.Entities;
+using JwtTokenLearning.Business.Services.Abstract;
 
 namespace JwtTokenLearning.Business.Services.Concrete
 {
-    class TokenService
+    public class TokenService:ITokenService
     {
+        public TokenDto CreateToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
